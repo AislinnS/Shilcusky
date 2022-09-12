d0d527e4971bd58896a928b5b6509ef7fcb4a366
AislinnS <shilcusky@susqu.edu>
2022-09-12 19:35:57 -0400

    Update Week2_Assignment.R

--- Week2/Week2_Assignment.R
+++ Week2/Week2_Assignment.R
@@ -1,5 +1,40 @@
 # With the data frame you created last week you will:
 
+dolomedes <- c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o')
+pardosa <- c('w','w','w','w','w','y','y','y','y','y','n','n','n','n','n')
+pennsylvinica <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+tigrosa <- c(42,43,56,78,98,87,76,9,56,45,57,32,34,24,42)
+pirata <- c(1.1,1.2,1.3,1.4,1.5,2.1,2.2,2.3,2.4,2.5,3.1,3.2,3.3,3.4,3.5)
+
+data <- cbind(dolomedes, pardosa, pennsylvinica, tigrosa, pirata)
+data
+
+df <- as.data.frame(data)
+df
+
+colnames(df) <- c('Dolomedes', 'Pardosa','Pennsylvinica','Tigrosa','Pirata')
+df
+
+row.names(df) <- df$Dolomedes
+df
+
+df[,-1]
+
+add.row <- data.frame('p',19, 'm', 13, 72)
+
+colnames(add.row) <- colnames(df)
+
+df1 <- rbind(df, add.row)
+df1
+
+colnames(df1) <- c('Dolomedes', 'Pardosa','Pennsylvinica','Tigrosa','Pirata')
+df
+
+row.names(df1) <- df1$Dolomedes
+df1
+
+df1[,-1]
+
 # Create a barplot for one numeric column, grouped by the character vector with 3 unique values
   # Add error bars with mean and standard deviation to the plot
   # Change the x and y labels and add a title
