b2c5021a5453207727e309795679cd62b52cf6ad
AislinnS <shilcusky@susqu.edu>
2022-10-19 18:26:13 -0400

    Week 7 progress
    Ah.

--- Week7/Week7_Population_Assignment.R
+++ Week7/Week7_Population_Assignment.R
@@ -1,33 +1,71 @@
 # Load the "anytime" and "ggplot2" packages to complete this week's assignment.
-
 # Read the "Plankton_move_average" CSV in from GitHub. 
+
+df <- read.csv("Plankton_move_average.csv")
+View(Plankton_move_average)
+
 # These are data from the Great Lakes Environmental Research Laboratory plankton sampling.
 
+
 #Used the following lines to format the date and remove NAs from the dataset:
-data$Date <- as.Date(data$Date, origin = "0001-01-01") # Setting values to "day zero".
-data <- na.omit(data)
+df$Date <- as.Date(df$Date, origin = 0001-01-01) # Setting values to "day zero".
+df <- na.omit(df)
+
+
 
 #Plot these population data over time with the following code:
-ggplot(data)  +
+ggplot(df)  +
   xlab("Numeric Date") + ylab("Density Individuals")+
-  geom_line(data=data, aes(Date, D.mendotae), color="black", alpha = 0.7, size=1)+
-  geom_line(data=data, aes(Date, LimncalanusF+LimncalanusM), color="orange",  alpha = 0.7, size=1)+ # adding males and females together, hint: this is actually spelled Limnocalanus
-  geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
-  geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
+  geom_line(data=df, aes(Date, D.mendotae), color="black", alpha = 0.7, size=1)+
+  geom_line(data=df, aes(Date, LimncalanusF+LimncalanusM), color="orange",  alpha = 0.7, size=1)+ # adding males and females together, hint: this is actually spelled Limnocalanus
+  geom_line(data=df, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
+  geom_line(data=df, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
   theme_bw() 
+
 
 # Export this plot to have on hand for reference in the next section of the assignment (and upload with your script).
 
 # (1) - Which species is most likely to be r-selected prey and which its primary predator? (2 pts)
+
+D. mendotae is most likely to be r-selected prey and its primary predator would be Limnocalanus.
+
 # What is one relationship the third species MIGHT have to the first two? (2 pts)
+
+It may serve as a confounding variable that affects the relationship between the first two species.
 
 #Now copy/paste in the Lotka-Volterra function, plotting script, and load the "deSolve" package from the tutorial:
 
+LotVmod <- function (Time, State, Pars) {
+  with(as.list(c(State, Pars)), {
+    dx = x*(alpha - beta*y)
+    dy = -y*(gamma - delta*x)
+    return(list(c(dx, dy)))
+  })
+}
+
+
+matplot(df[,-1], type = "l", xlab = "time", ylab = "population")
+legend("topright", c("Limncalanus", "D.mendotae"), lty = c(1,2), col = c(1,2), box.lwd = 0)
+
+
+library(deSolve)
+
+
 # (2) - What do alpha, beta, gamma, and delta represent in this function? (4 pts)
+
+Alpha represents how well Limncalanus competes with D.mendotae. Beta represents how well D.mendotae competes with Limcalanus. Delta represents the rate of Limncalanus consumption/die off.
 
 # (3) - By only changing values for alpha, beta, gamma, and/or delta
 # change the default parameters of the L-V model to best approximate the relationship between Limncalanus and D.mendotae, assuming both plots are on the same time scale.
+
+library(EcoVirtual)
+compLV(n01=10, n02=10,r1=0.05, r2=0.03, k1=80, k2=50, alfa=1.2, beta=0.5, tmax=200)
+compLV(n01=10, n02=10,r1=0.05, r2=0.03, k1=80, k2=50, alfa=1.2, beta=0.3, tmax=400)
+
 # What are the changes you've made to alpha, beta, gamma, and delta from the default values; and what do they say in a relative sense about the plankton data? (4 pts)
+
+I changed the beta value, making species 2 a better competititor, showing a longer growth rate to the population trajectory.
+
 # Are there other paramenter changes that could have created the same end result? (2 pts)
 # Export your final L-V plot with a legend that includes the appropriate genus and/or species name as if the model results were the real plankton data, 
 # and upload with your script. (hint - remember which one is the predator and which is the prey)
