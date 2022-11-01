0fe7488aad46a90b6624ddfd0f1e3dec00a7096d
AislinnS <shilcusky@susqu.edu>
2022-11-01 00:47:47 -0400

    Week 9 eek
    eek

--- Week9/Week9_Ecosystems_Assignment.R
+++ Week9/Week9_Ecosystems_Assignment.R
@@ -2,45 +2,51 @@
   # There are at least five options in the dataset to create the following comparisons.
 
 library(readxl)
-vegetation.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Vegetation_transects")
-vegetation <- as.data.frame(vegetation.tibble)
 
 invertebrate.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
 invertebrate <- as.data.frame(invertebrate.tibble)
 
-head(vegetation)
+abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
+abiotic <- as.data.frame(abiotic.tibble)
+
 head(invertebrate)
-
-boxplot(pH ~ Site, data= abiotic, main = "pH by Parcel")
-boxplot(pH ~ Land_Use, data= abiotic, main = "pH by Land Use")
-abiotic.names <- paste(abiotic$Site, abiotic$Land_Use, abiotic$Plot)
-
-vegetation.names <- paste(vegetation$Land_Use, vegetation$Parcel)
-vegetation$names <- vegetation.names
+head(abiotic)
 
 invertebrate.names <- paste(invertebrate$Land_use, invertebrate$Parcel)
 invertebrate$names <- invertebrate.names
 
-vegetation.means <- aggregate(x = vegetation, by = list(vegetation$names), FUN = "mean")
-head(vegetation.means)
+abiotic.names <- paste(abiotic$Land_Use, abiotic$Parcel)
+abiotic$names <- abiotic.names
 
 invertebrate.means <- aggregate(x = invertebrate, by = list(invertebrate$names), FUN = "mean")
 head(invertebrate.means)
 
+abiotic.means <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
+head(abiotic.means)
 
-
-maybe use abiotic instead of vegetation
-find column to replace something with
 # (Q1 - 12 pts) Use the dataset from the tutorial to complete one redundancy analysis (RDA) with variance partitioning on a different community (NOT the nematodes).
   # Explain the ecological importance of your significant predictor variables, or the importance if none are significant for your community.
 
-abiotic$names==invert$names
-[,4:12]
-th.np <-df[,c.-2,-3,-5]
+invertebrate.means1 <- invertebrate.means[-5,c(-1:-3,-73)]
+invertebrate.means2 <- as.data.frame(sapply(invertebrate.means1, as.numeric ))
+
+abiotic.means1 <- abiotic.means[,c(-1,-2,-3,-5,-6,-16)]
+abiotic.means2 <- as.data.frame(sapply(abiotic.means1, as.numeric ))
+
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
-  # Explain the ecological importance of the significant predictors, or lack of significant predictors.
+
+library(vegan)
+colnames(abiotic.means2)
+ord <- rda(invertebrate.means2 ~ totalN, abiotic.means2)
+plot(ord)
+anova(ord)
+
+fit.nbinom <- fitdist(soil.plants$Leaves, distr = "nbinom")
+
+# Explain the ecological importance of the significant predictors, or lack of significant predictors.
+
+The f value typically shows significance, but nitrogen, our predictor variable, does not show statistical significance at 0.7111.
 
 # (Q3 - 6 pts) Provide a 3-4 sentence synthesis of how these results relate to one another and the value of considering both together for interpreting biotic-abiotic interactions.
-
 
