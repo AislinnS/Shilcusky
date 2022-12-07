5290b444e0f3b0419f8b2b80be4257ac6bad5602
AislinnS <shilcusky@susqu.edu>
2022-12-07 17:20:25 -0500

    dYSDERA 12.7.22

--- Dysdera Outline.R
+++ Dysdera Outline.R
@@ -1,12 +1,20 @@
-setwd("C:/GitHub/Shilcusky/Dysdera.csv")
+setwd("C:/Users/shilcusky/Documents/GitHub/Shilcusky/Dysdera Project")
 library(readcsv)
 
 library(spdep)
 library(adespatial)
 library(vegan)
 
-df <- read.csv("Dysdera.csv", header =T)
-Dysdera.mat <- as.matrix(df)
 
-Dysdera.mat[,c(-2,-3,-5,-8,-9)]
-group <- Dysdera.mat[,c(-2,-3,-5,-8,-9)]
+df1 <- read.csv("Dysdera.csv", header =T)
+Dysdera <- df1[,c(-2,-3,-5,-8,-9)]
+subset(Dysdera, Dysdera$Island == "Lanzarote")
+
+
+df2 <- read.csv("Dysdera Artificial Brightness.csv", header =T)
+Brightness.mat <- as.matrix(df2)
+
+
+merge(Dysdera,Dysdera_Artificial_Brightness, by=c ("Latitude","Longitude"))
+      
+      
